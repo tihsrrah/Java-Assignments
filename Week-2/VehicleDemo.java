1efad61d9f820cefb08f4f7f92c83310b3d4d09a
Harshit Verma <142871937+tihsrrah@users.noreply.github.com>
2026-07-16 19:40:02 +0530

    Add VehicleDemo class with main method

---
+++ Week-2/VehicleDemo.java
@@ -0,0 +1,11 @@
+public class VehicleDemo {
+
+    public static void main(String[] args) {
+
+        Vehicle v1 = new Car();
+        Vehicle v2 = new Bike();
+
+        v1.start();
+        v2.start();
+    }
+}
