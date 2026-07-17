2ba33c52074da84b346632860f937a30868e8fda
Harshit Verma <142871937+tihsrrah@users.noreply.github.com>
2026-07-17 11:59:15 +0530

    Create Main.java

---
+++ Capstone-Project/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("=======================================");
+        System.out.println(" Welcome to Student Management System");
+        System.out.println("=======================================");
+
+        StudentManagement system = new StudentManagement();
+
+        system.start();
+    }
+}
